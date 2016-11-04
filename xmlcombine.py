43e8fee43fce13a697b7ddb2790742d6c4d2ff1b
Mikhael-Danilov <mikhaeldanilov@gmail.com>
2016-11-04 21:17:49 +0300

    Add files via upload

--- xmlcombine.py
+++ xmlcombine.py
@@ -1,31 +1,29 @@
 #!/usr/bin/env python
 import sys
+import os
 from xml.etree import ElementTree
 
-def run(files):
-    first = None
-    for filename in files:
-        data = ElementTree.parse(filename).getroot()
-        if first is None:
-            first = data
-        else:
-            first.extend(data)
-            
-    entries_already_seen = {}
-    entries_to_remove = []
-    for entry in first:
-      entry_name = entry.attrib['name']
-      if entry_name in entries_already_seen:
-         entries_to_remove.append(entry)
-      else:
-         entries_already_seen[entry_name] = True
-      
-    for entry in entries_to_remove:
-       first.remove(entry)
-      
-    if first is not None:
-        print ElementTree.tostring(first)
-        ElementTree.ElementTree(first).write("out.xml","utf-8")
+def combine(resource_name):
+    first = ElementTree.parse("master/"+resource_name).getroot()
+    
+    if os.path.isfile("slave/"+resource_name):
+       first.extend(ElementTree.parse("slave/"+resource_name).getroot())
+       
+       entries_already_seen = {}
+       entries_to_remove = []
+       for entry in first:
+         entry_name = entry.attrib['name']
+         if entry_name in entries_already_seen:
+            entries_to_remove.append(entry)
+         else:
+            entries_already_seen[entry_name] = True
+         
+       for entry in entries_to_remove:
+          first.remove(entry)
+    
+    ElementTree.ElementTree(first).write("combined/"+resource_name,"utf-8",xml_declaration=True)
 
-if __name__ == "__main__":
-    run(sys.argv[1:])
+
+for filename in os.listdir("master"):
+    if filename.endswith(".xml"):
+      combine(filename)
