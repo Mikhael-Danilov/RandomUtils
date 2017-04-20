7cbc81a4e022c9f65b9bdee334d46feca574f2a8
Mikhael-Danilov <mikhaeldanilov@gmail.com>
2017-04-20 22:37:44 +0300

    Add files via upload

--- tx2as.py
+++ tx2as.py
@@ -2,13 +2,20 @@
 import os
 import shutil
 import string
-
+import pprint
 from xml.etree import ElementTree
+
+dstDir = "/cygdrive/e/Nyrds/Remixed Pixel Dungeon/PixelDungeon/src/main/res/"
 
 xml_ext = '.xml'
 translations_dir = 'translations/'
 
 locale_remap = {'de_DE':'de', 'es_ES':'es', 'fr_FR':'fr', 'pl_PL':'pl', 'nl_NL':'nl', 'ro_RO':'ro', 'ru_RU':'ru','uk_UA':'uk','pt_BR':'pt-rBR','pt_PT':'pt-rPT','es_MX':'es-rMX'}
+
+used_locales = {'de','es','fr','it','ja','pl','pt-rBr','ru','tr','uk','zh'}
+
+counters = {}
+totalCounter = {}
 
 class CommentedTreeBuilder ( ElementTree.XMLTreeBuilder ):
    def __init__ ( self, html = 0, target = None ):
@@ -24,19 +31,44 @@ for _, dirs, _ in os.walk(translations_dir):
     for dir_name in dirs:
       resource_name = string.split(dir_name,'.')[1][:-3]+xml_ext
       print "Processing:", resource_name
+      
+      counters[resource_name] = {}
+      
       for _, _, files in os.walk(translations_dir+dir_name):
          for file_name in files:
             locale_code = file_name[:-4]
+            
             if locale_code in locale_remap:
                locale_code = locale_remap[locale_code]
-            resource_dir = "android/values-"+locale_code
+            
+            if not locale_code in used_locales:
+               continue
+            
+            if locale_code not in totalCounter:
+               totalCounter[locale_code] = 0
+               
+            counters[resource_name][locale_code] = 0
+            
+            resource_dir = dstDir + "values-"+locale_code
             if not os.path.isdir(resource_dir):
                os.makedirs(resource_dir)
             
-            transifexData = ElementTree.parse(translations_dir+dir_name+"/"+file_name, parser = CommentedTreeBuilder() ).getroot()
             
-            for entry in transifexData:
-               if entry.text is not None and "\\\\" in entry.text:
-                  entry.text=entry.text.replace("\\\\","\\")
+            currentFilePath = translations_dir+dir_name+"/"+file_name
             
-            ElementTree.ElementTree(transifexData).write(resource_dir+"/"+resource_name,"utf-8",xml_declaration=True)
+            #print currentFilePath
+            try:
+               transifexData = ElementTree.parse(currentFilePath, parser = CommentedTreeBuilder() ).getroot()
+               
+               for entry in transifexData:
+                  counters[resource_name][locale_code]+=1
+                  totalCounter[locale_code]+=1
+                  if entry.text is not None and "\\\\" in entry.text:
+                     entry.text=entry.text.replace("\\\\","\\")
+               
+               ElementTree.ElementTree(transifexData).write(resource_dir+"/"+resource_name,"utf-8",xml_declaration=True)
+               
+            except ElementTree.ParseError:
+               print "shit happens with " + currentFilePath
+            
+pprint.pprint(totalCounter)
