ba72022d7ef6d7804678da814287b4dd28878ad3
Mikhael Danilov <mikhaeldanilov@gmail.com>
2019-04-10 00:19:17 +0300

    sync...

--- xmlcombine.py
+++ xmlcombine.py
@@ -1,29 +1,26 @@
 #!/usr/bin/env python
 import sys
 import os
+import string
+import pprint
 from xml.etree import ElementTree
 
-def combine(resource_name):
-    first = ElementTree.parse("master/"+resource_name).getroot()
-    
-    if os.path.isfile("slave/"+resource_name):
-       first.extend(ElementTree.parse("slave/"+resource_name).getroot())
-       
-       entries_already_seen = {}
-       entries_to_remove = []
-       for entry in first:
-         entry_name = entry.attrib['name']
-         if entry_name in entries_already_seen:
-            entries_to_remove.append(entry)
-         else:
-            entries_already_seen[entry_name] = True
-         
-       for entry in entries_to_remove:
-          first.remove(entry)
-    
-    ElementTree.ElementTree(first).write("combined/"+resource_name,"utf-8",xml_declaration=True)
+path = "/media/mike/BigData/Nyrds/pixel-dungeon-remix/RemixedDungeon/src/main/res"
 
+combined = None
 
-for filename in os.listdir("master"):
-    if filename.endswith(".xml"):
-      combine(filename)
+ignored_files = {"strings_not_translate.xml","strings_not_translate.xml","strings_api_signature.xml","strings_all.xml","strings-all-xml--mas.xml"}
+
+for dir in os.listdir(path):
+    langPath = path + "/" + dir
+    combined = None
+    for filename in os.listdir(langPath):
+        if filename.endswith(".xml") and filename.startswith("str") and filename not in ignored_files:
+            fullPath = langPath + "/" + filename
+            print (fullPath)
+            if combined is None:
+                combined = ElementTree.parse(fullPath).getroot()
+            else:
+                combined.extend(ElementTree.parse(fullPath).getroot())
+    if combined is not None:
+        ElementTree.ElementTree(combined).write("strings_"+dir+".xml","utf-8",xml_declaration=True)
